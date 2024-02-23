9e05db3f8af1b8ecce1789885300fa5abedb384e
DeusNexus <nextgenage@gmail.com>
2024-02-23 22:02:39 +0800

    Notebooks Update

---
+++ data/processed_data/processing_script.py
@@ -0,0 +1,5 @@
+################################################################
+##### Store processed data such that it can be historized! #####
+################################################################
+
+# Store the data in a Data Mart? The original data can easily be accessed and processed data can be accessed aswell to see how changes over time affect model re-training!
