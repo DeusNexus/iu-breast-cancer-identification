f427b73a843cfeadcd4c711c2ca2900d738fdafa
DeusNexus <nextgenage@gmail.com>
2024-02-26 21:54:20 +0800

    Fixed MongoDB Insertion & Best Model Training

---
+++ scripts/database.py
@@ -0,0 +1,179 @@
+from pymongo import MongoClient
+from pymongo.errors import WriteError
+from datetime import datetime
+from typing import Dict, List, Union
+import pandas as pd
+
+def connect_db(
+        url='mongodb://localhost:27017/',
+        db_name='breast_cancer_mlops'
+    ):
+    """
+    Establishes a connection to the MongoDB database.
+    
+    Parameters:
+    - url (str): MongoDB connection URL.
+    - db_name (str): Name of the database to connect to.
+    
+    Returns:
+    - db: A reference to the specified MongoDB database.
+    """
+    # Establish a connection to the MongoDB server
+    client = MongoClient(url)
+
+    # Select your database
+    db = client[db_name]
+
+    return db
+
+def close_db(db):
+    """
+    Closes the connection to the MongoDB database.
+    
+    Parameters:
+    - db: The database connection to close.
+    
+    Returns:
+    - None
+    """
+    # Close client connection to database
+    return db.close()
+
+def insert_original_document(
+        db: MongoClient, 
+        data_id: Union[str], 
+        features: Dict[str, Union[int, float, str, None]], 
+        collection_date: datetime, 
+        source: str, 
+        dataset_label: str
+    ) -> bool:
+    
+    """
+    Inserts a new document into the original data collection.
+    
+    Parameters:
+    - db: The database connection.
+    - data_id (str): Unique identifier for the data.
+    - features (dict): Feature values of the data.
+    - collection_date (str): Date when the data was collected.
+    - source (str): Source of the data.
+    - dataset_label (str): Label identifying the dataset.
+    
+    Returns:
+    - bool: True if insertion was successful, False otherwise.
+    """
+    # Example:
+    # insert_original_document(
+    #     db, 
+    #     data_id='12345', 
+    #     features={}, 
+    #     collection_date='2024-02-25', 
+    #     source='Hospital XYZ', 
+    #     dataset_label='Breast_Cancer_Study_2024'
+    # )
+
+    collection = db['original_data']
+
+    # Example document following the original data schema
+    document = {
+        "data_id": data_id, #"12345",
+        "features": features, # Feature values here
+        "metadata": {
+            "collection_date": collection_date, #"2024-02-25"
+            "source": source, #"Hospital XYZ"
+            "dataset_label": dataset_label, #"Breast_Cancer_Study_2024"
+        }
+    }
+
+    # Handle 'nan' and convert 'datetime' to 'string'
+    for key, value in document['features'].items():
+        if pd.isna(value):
+            document['features'][key] = ''  # Replace 'nan' with 0 or other appropriate value
+
+    document['metadata']['collection_date'] = document['metadata']['collection_date'].strftime('%Y-%m-%d')
+
+    # Now you can attempt to insert the document again
+
+    print(document)
+
+    try:
+        # Insert the document into the specified collection
+        result = collection.insert_one(document)
+        print(f"Document inserted with _id: {result.inserted_id}")
+        return True
+    except WriteError as e:
+        print(f"Document failed validation: {e}")
+        return False
+
+def insert_processed_document(
+        db: MongoClient, 
+        processed_data_id: Union[int, str], 
+        original_data_id: Union[int, str], 
+        features: Dict[str, Union[int, float, str]], 
+        steps: List[str], 
+        processed_on: datetime, 
+        processed_by: str, 
+        dataset_label: str, 
+        comments: str
+    ) -> bool:
+      
+    """
+    Inserts a new document into the processed data collection.
+    
+    Parameters:
+    - db: The database connection.
+    - processed_data_id (str): Unique identifier for the processed data.
+    - original_data_id (str): Identifier linking back to the original raw data.
+    - features (dict): Processed feature values.
+    - steps (list): List of processing steps applied to the data.
+    - processed_on (str): Date when the data was processed.
+    - processed_by (str): Identifier of the person or system that processed the data.
+    - dataset_label (str): Label identifying the processed dataset.
+    - comments (str): Additional comments regarding the processing.
+    
+    Returns:
+    - bool: True if insertion was successful, False otherwise.
+    """
+
+    # Example:
+    # insert_processed_document(
+    #     db, 
+    #     processed_data_id='67890', 
+    #     original_data_id='12345', 
+    #     features={}, 
+    #     steps=["scaled", "encoded"], 
+    #     processed_on="2024-03-01", 
+    #     processed_by="DataEngineerA", 
+    #     collection_date='2024-02-25', 
+    #     dataset_label='Breast_Cancer_Study_2024_Processed', 
+    #     comments='Data scaled and encoded for model input.'
+    # )
+
+    collection = db['processed_data']
+    
+    # Example document following the processed data schema
+    document = {
+        "processed_data_id": processed_data_id, #"67890"
+        "original_data_id": original_data_id, #"12345"
+        "features": features, # Processed feature values here
+        "processing_details": {
+            "steps": steps, #["scaled", "encoded"],
+            "processed_on": processed_on, #"2024-03-01",
+            "processed_by": processed_by, #"DataEngineerA"
+        },
+        "metadata": {
+            "dataset_label": dataset_label, #"Breast_Cancer_Study_2024_Processed",
+            "comments": comments, #"Data scaled and encoded for model input."
+        }
+    }
+
+    print(document)
+
+    try:
+        # Insert the document into the specified collection
+        result = collection.insert_one(document)
+        print(f"Document inserted with _id: {result.inserted_id}")
+        return True
+    except WriteError as e:
+        print(f"Document failed validation: {e}")
+        return False
---
+++ scripts/write_original_data.py
@@ -0,0 +1,44 @@
+from database import connect_db, close_db, insert_original_document, insert_processed_document
+from datetime import datetime
+from functools import wraps
+import pandas as pd
+import uuid
+
+def mongo_connection(func):
+    @wraps(func)
+    def with_connection(*args, **kwargs):
+        db = connect_db()
+        try:
+            result = func(db, *args, **kwargs)
+        finally:
+            close_db(db.client)  # Assuming db.close() is meant to close the MongoClient
+        return result
+    return with_connection
+
+@mongo_connection
+def db_operations(db, *args, **kwargs):
+
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv('../data/original_data/dataset.csv')
+    now = datetime.now() # Current datetime
+    data_uuid = uuid.uuid4() # One data_id for entire dataset
+
+    for index, row in df.iterrows():
+        data_id = str(data_uuid)
+        features = row.to_dict()
+        collection_date = now  # Adjust if you have a specific column for the date
+        source = "https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29"  # Define as needed
+        dataset_label = "Breast Cancer Wisconsin Diagnostic"  # Define as needed
+
+        insert_original_document(
+            db=db, 
+            data_id=data_id, 
+            features=features, 
+            collection_date=collection_date, 
+            source=source, 
+            dataset_label=dataset_label
+        )
+
+    pass
+
+db_operations()
